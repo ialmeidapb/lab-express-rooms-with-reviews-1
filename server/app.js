3a738ae1aa9823e91fd5552faf5bbcba40097716
ialmeida <ialmeida.pb@gmail.com>
2021-05-10 18:38:45 -0300

    done

---
+++ server/app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const PORT = 4000;
+
+
+// Importando as configurações do banco de dados e inicializando a conexão
+const db = require("./config/db.config");
+db();
+
+// Configurar o express para entender requisições contendo JSON no corpo
+app.use(express.json());
+
+
+// Inicia o servidor para escutar requisições HTTP na porta 4000
+app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
